3d3aacc11b5e94afe70c7adbc4d12e0abf93d29b
zwx08 <zwx080104@gmail.com>
2024-07-29 18:15:22 +0800

    添加测试案例

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import httpx
 
 # 定义要发送的数据
-data = {"id": "1234", "data": "TestItem"}
+data = {"id": "123456", "data": "TestItem"}
 
-# 使用httpx发送POST请求
+# 写入
 #response = httpx.get("http://127.0.0.1:8084", params=data)
-response = httpx.post("http://127.0.0.1:8084", params=data)
+#response = httpx.get("https://:8", params=data, verify=False)
+#response = httpx.post("https://:8", params=data, verify=False)
+
+# 查询
+response = httpx.get("https://:8/12345", verify=False)
+
 # 打印响应状态码和主体内容
 print(f"Status Code: {response.status_code}")
 print(f"Response Body: {response.json()}")
